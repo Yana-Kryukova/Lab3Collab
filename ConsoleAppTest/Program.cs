8f4b1a1370b269d0d9c34e486aca06c8ed1e925f
Yana-Kryukova <krjanik@icloud.com>
2023-05-03 19:35:51 +0700

    First loop

--- ConsoleAppTest/Program.cs
+++ ConsoleAppTest/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int A = 0, B = 0;
+double res = 0;
+string s = "";
+Console.WriteLine("Enter A, B, +, -, *, or /");
+s = Console.ReadLine();
+switch (s)
+{
+    case "A":Input(ref A); break;
+    case "B": Input(ref B); break;
+    case "+": res = Plus(A, B); break;
+    case "-": res = Minus(A, B); break;
+    case "*": res = Multy(A, B); break;
+    case "/": res = Dev(A, B); break;
+}
+Console.WriteLine($"{A} {s} {B} = {res}");
